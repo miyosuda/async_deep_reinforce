aaaa557e65d1e467a3186dff0171c8b44842c332
kosuke miyoshi <miyoshi@narr.jp>
2016-04-16 22:09:09 +0900

    change ENTROPY_BETA param as a test

--- constants.py
+++ constants.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 
-#GAMMA = 0.95
-GAMMA = 0.99 # discount factor for rewards
 LOCAL_T_MAX = 5 # repeat step size
 RMSP_EPSILON = 1e-10 # epsilon parameter for RMSProp
-ENTROPY_BETA = 0.001 # entropy regurarlization constant
-
-ROM = "pong.bin"     # action size = 3
-#ROM = "breakout.bin" # action size = 4
-
-ACTION_SIZE = 3 # action size
-
-PARALLEL_SIZE = 8 # parallel thread size
 CHECKPOINT_DIR = 'checkpoints'
-
 INITIAL_ALPHA_LOW = 1e-4    # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e-2   # log_uniform high limit for learning rate
+
+PARALLEL_SIZE = 8 # parallel thread size
+ROM = "pong.bin"     # action size = 3
+#ROM = "breakout.bin" # action size = 4
+ACTION_SIZE = 3 # action size
+
 INITIAL_ALPHA_LOG_RATE = 0.5 # log_uniform interpolate rate for learning rate
-#INITIAL_ALPHA_LOG_RATE = 0.25
+GAMMA = 0.99 # discount factor for rewards
+ENTROPY_BETA = 0.01 # entropy regurarlization constant
 MAX_TIME_STEP = 4000000 * 8 # 
