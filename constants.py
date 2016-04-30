c75591e53ef6feb0fd09d1fd1b7b163674d0aaa9
kosuke miyoshi <miyoshi@narr.jp>
2016-04-30 18:22:51 +0900

    trying RMSProp epsilon 0.1

--- constants.py
+++ constants.py
@@ -2,7 +2,8 @@
 
 LOCAL_T_MAX = 17 # repeat step size
 RMSP_ALPHA = 0.99 # decap parameter for RMSProp
-RMSP_EPSILON = 1e-10 # epsilon parameter for RMSProp
+#RMSP_EPSILON = 1e-10 # epsilon parameter for RMSProp
+RMSP_EPSILON = 0.1 # epsilon parameter for RMSProp
 CHECKPOINT_DIR = 'checkpoints'
 LOG_FILE = 'tmp/a3c_log'
 INITIAL_ALPHA_LOW = 1e-4    # log_uniform low limit for learning rate
@@ -14,6 +15,6 @@ ACTION_SIZE = 3 # action size
 
 INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 GAMMA = 0.99 # discount factor for rewards
-ENTROPY_BETA = 0.1 # entropy regurarlization constant
+ENTROPY_BETA = 0.01 # entropy regurarlization constant
 MAX_TIME_STEP = 6 * 10**7
 GRAD_NORM_CLIP = 40.0 # gradient norm clipping
